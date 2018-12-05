3c55d8a84aab5dd0632d8fe62374ca4139b88889
Rebecca Lee <13988165+beccaree@users.noreply.github.com>
2018-12-05 17:09:58 +1300

    Update day4-1.py

--- advent2018/day4-1.py
+++ advent2018/day4-1.py
@@ -1,21 +1,36 @@
 import sys
 import re
+import operator
 
 logs = []
+guardId = 0
+minuteAsleep = 0
 
 sys.stdin = open('input.txt', 'r')
 # [1518-11-01 00:05] falls asleep
 for line in sys.stdin:
+	#line = '[1518-11-01 00:05] falls asleep'
 	m = re.search('\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\]', line)
+	hour = int(m.group(4))
+	if hour == 0: #mignight
+		hour = 24
 	log = {
-		"year": int(m.group(1))
-		"month": int(m.group(2))
-		"day": int(m.group(3))
-		"hour": int(m.group(4))
-		"minute": int(m.group(5))
-		"activity": line[(line.find(']')+2):] #including space
+		"year": int(m.group(1)),
+		"month": int(m.group(2)),
+		"day": int(m.group(3)),
+		"hour": hour,
+		"minute": int(m.group(5)),
+		"detail": line[(line.find(']')+2):] #including space
 	}
+	logs.append(log)
+	
+# sort the logs by date time
+logs.sort(key = operator.itemgetter("day", "hour", "minute"))
 
+print(logs)
+
+# split logs into guard specific and record sleep time
+# array of minutes per guard
 
 print("done")	
 
@@ -24,4 +39,4 @@ print("done")
 #hour = int(m.group(4))
 #month = int(m.group(2))
 #minute = int(m.group(5))
-#activity = line[(line.find(']')+2):] #including space
+#detail = line[(line.find(']')+2):] #including space

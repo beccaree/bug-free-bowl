24136cd7a4ec52dff093f4b0180e484f5eac72f3
Rebecca Lee <13988165+beccaree@users.noreply.github.com>
2018-12-09 01:44:22 +1300

    Update and rename day8-1.py to day8.py

--- advent2018/day8.py
+++ advent2018/day8.py
@@ -9,28 +9,40 @@ for line in sys.stdin:
 	tree = [int(x) for x in line.split(" ")]
 
 totalMetadata = 0
+totalValue = 0
 
 def nodeLength(segment):
 	global totalMetadata
-	print(segment)
+	global totalValue
 	numChild = int(segment[0])
 	numMetadata = int(segment[1])
 	result = 2 #first 2 data points is header
 	if numChild == 0:
+		v = 0
 		for x in range(numMetadata):
 			totalMetadata += segment[2+x]
+			v += segment[2+x]
 		result += numMetadata
-		return result
+		return [result, v]
 	else:
 		children = segment[result:len(segment)-numMetadata]
 		length = 0
+		childValues = []
+		v = 0
 		for i in range(numChild):
-			l = nodeLength(children[length:])
+			f = nodeLength(children[length:])
+			l = f[0]
+			childValues.append(f[1])
 			length += l
 		for y in range(numMetadata):
 			totalMetadata += segment[2+length+y]
-		return result + length + numMetadata
+			a = segment[2+length+y]
+			if a <= numChild:
+				v += childValues[a-1]
+		return [result + length + numMetadata, v]
 
-nodeLength(tree)
+result = nodeLength(tree)
+
 print(totalMetadata)
+print(result[1])
 print("done")
